53e578c7b3db44de5904b46c167f2265e201e92e
git_Vickyprinz <kariukivictor315@gmail.com>
2022-11-25 11:58:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let name = ['Hunter', 'sinon', 'Percey']
+
+function writeCards(name , event) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = 0
+    while (i <= number) {
+        console.log(number--)
+    }
+}
